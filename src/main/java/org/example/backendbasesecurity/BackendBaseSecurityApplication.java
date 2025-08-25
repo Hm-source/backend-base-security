12facb8dc12eecc2de8d36f5b308ce7f2427ee33
Hm-source <swo8852@gmail.com>
2025-08-25 14:08:09 +0900

    first commit

---
+++ src/main/java/org/example/backendbasesecurity/BackendBaseSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendbasesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaseSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaseSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/backendbasesecurity/BackendBaseSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendbasesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBaseSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
